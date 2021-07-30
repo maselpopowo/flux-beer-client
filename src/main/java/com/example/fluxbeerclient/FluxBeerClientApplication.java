4abd481ef44c3cd4a90a7aa43bf5ad912a976146
Marcin Kunicki <masel.popowo@gmail.com>
2021-07-30 12:53:34 +0200

    Init commit

---
+++ src/main/java/com/example/fluxbeerclient/FluxBeerClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.fluxbeerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxBeerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxBeerClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fluxbeerclient/FluxBeerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fluxbeerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxBeerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
